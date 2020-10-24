8e72b66434b81f37df6bc988ba6291d9c5575956
Quinten <58103738+HexaOneOfficial@users.noreply.github.com>
2020-10-24 09:40:39 +0200

    patch pangolin.zip

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 ## version 201004 
 
-           sudo apt-get install -y matchbox-window-manager
+            wget https://github.com/HexaOneOfficial/pangolin-linux/releases/download/201004/Pangolin.zip
+            sudo apt-get install -y matchbox-window-manager
             sudo rm /etc/apt/preferences.d/nosnap.pref
             sudo apt install snapd
             sudo snap install flutter --classic
