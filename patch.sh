03078bccf27f1f7ee9f524665a2c5f831c92e535
Quinten <58103738+HexaOneOfficial@users.noreply.github.com>
2020-12-31 15:17:38 +0000

    Delete patch.sh

--- patch.sh
+++
@@ -1,5 +0,0 @@
-# this patch doesn't work atm
-# what this patch does is scaling to the right resolution
-feh --bg-center /dahlia/boot-logo.png&
-matchbox-window-manager -use_titlebar no &
-exec /dahlia/pangolin_desktop/pangolin_desktop
